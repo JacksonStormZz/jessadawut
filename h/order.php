bef136b9020155f91683d1d806ded0796a17ac39
JacksonStormZz <jassadawut.2007@gmail.com>
2026-02-04 16:09:53 +0700

    a

---
+++ h/order.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+if (empty($_SESSION|['aid'])) {
+echo "Access Denied";
+echo "<meta http-equiv='refresh' content='3; url=index.php'";
+exit;
+}
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>เจษฎาวุฒิ มั่นยืน(ฟลุ๊ค)</title>
+</head>
+
+<body>
+<h1> จัดการออเดอร์ เจษฎาวุฒิ มั่นยืน(ฟลุ๊ค)</h1>
+
+<?php echo $_SESSION['a_name'];?><br>
+<ul>
+<a href = "product.php"><li>จัดการสินค้า </li></a>
+<a href = "order.php"><li>จัดการออเดอร์</li></a>
+<a href = "customers.php"><li>จัดการลูกค้า</li></a>
+<a href = "product.php"><li>ออกจากระบบ</li></a>
+
+</body>
+</html>
