14f6e236e9023071873c8a62dfd7b1eae8001acd
JacksonStormZz <jassadawut.2007@gmail.com>
2026-02-04 17:20:27 +0700

    ฟ

--- h/index.php
+++ h/index.php
@@ -1,48 +1,39 @@
-<?php
-	session_start();
-?>
-<meta charset="utf-8">
-<title>เจษฎาวุฒิ มั่นยืน (ฟลุ๊ค)<!doctype html>
+<!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
-<title>เจษฎาวุฒิ มั่นยืน (ฟลุ๊ค)</title>
+<title>เจษฎาวุฒิ มั่นยืน </title>
 </head>
 
 <body>
-<h1>หน้าเข้าสู่ระบบ - เจษฎาวุฒิ มั่นยืน</h1>
+<h1>เข้าสู่ระบบหลังบ้าน - เจษฎาวุฒิ</h1>
 
-<form method="post" action="">
-Username <input type="text" name="auser" autofocus required> <br>
-Password <input type="password" name="apwd" required> <br>
-<button type="submit" name="Submit">LOGIN</button></form>
+<form method ="post" action="">
+Username <input type="text" name="auser" autofocus required><br>
+Password <input type="password" name="apwd" required><br>
+<button type="submit" name="Submit">LOGIN</button>
+</form>
+
 <?php
-if(isset($_POST['Submit'])){
+if(isset($_POST['Submit'])) {
     include_once("connectdb.php");
-    $sql = "SELECT * FROM admin WHERE a_username='{$_POST['auser']}'AND a_password='{$_POST['apwd']}'LIMIT 1";
-    $rs = mysqli.query($conn,$sql);
+    $sql = "SELECT * FROM admin WHERE a_username ='{$_POST['auser']}' AND  a_password ='{$_POST['apwd']}' LIMIT 1" ;
+    $rs = mysqli_query($conn,$sql);
     $num = mysqli_num_rows($rs);
-
-    if($num==1){
+    
+    if ($num == 1) {
         $data = mysqli_fetch_array($rs);
         $_SESSION['aid'] = $data['a_id'];
-        $_SESSION['name'] = $data['a_name'];
-        echo"<script>";
-        echo"window.location='index2.php';";
-        echo"</script>";
-
-    }else{
-        echo "script";
+        $SESSION['aname']= $data['a_name'];
+        echo "<script>";
+        echo "window.location='index2.php';";
+        echo "</script>";
+    } else {
+        echo "<script>";
         echo "alert('Username หรือ Password ไม่ถูกต้อง');";
         echo "</script>";
     }
 }
 ?>
-</body>
-</html>
-</title>
-</head>
-
-<body>
 </body>
 </html>
