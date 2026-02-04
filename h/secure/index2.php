b0d88efa13dd52a40f5d0c93aadfe0cd84e5bd66
JacksonStormZz <jassadawut.2007@gmail.com>
2026-02-04 18:12:25 +0700

    5

--- h/secure/index2.php
+++ h/secure/index2.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ระบบหลังบ้าน -เจษฎาวุฒิ /title>
+    <title>ระบบหลังบ้าน - จุฬาลักษณ์</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;500&display=swap" rel="stylesheet">
     <style> body { font-family: 'Kanit', sans-serif; background-color: #f8f9fc; } </style>
